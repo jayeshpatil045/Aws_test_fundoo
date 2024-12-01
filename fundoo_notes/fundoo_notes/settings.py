b8d70a559f844db4ab1065699ac31862a869ab5e
Jayesh Patil <126756612+jayeshpatil045@users.noreply.github.com>
2024-12-01 15:18:20 +0530

    Update settings.py

--- fundoo_notes/fundoo_notes/settings.py
+++ fundoo_notes/fundoo_notes/settings.py
@@ -116,10 +116,10 @@ WSGI_APPLICATION = 'fundoo_notes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "user",
-        'USER': "postgres",
-        'PASSWORD': "7387837430",
-        'HOST': "3.110.41.81", 
+        'NAME': 'fundoo_db',
+        'USER': 'jayesh',
+        'PASSWORD': 'jaypatil',
+        'HOST': '172.31.95.95', 
         'PORT': '5432',
     }
 }
